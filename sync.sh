5986de453a2d84ee6edd336850192bb2efe54b66
Alex Manelis <amanelis@gmail.com>
2014-07-08 16:41:34 -0700

    Added more files

--- sync.sh
+++ sync.sh
@@ -1,3 +1,12 @@
-cp ~/.bashrc ./
-cp ~/.vimrc  ./
-cp ~/.zshrc  ./
+cp ~/.bash_profile ./
+cp ~/.bashrc    ./
+cp ~/.gemrc     ./
+cp ~/.gitconfig ./
+cp ~/.gitignore_global ./
+cp ~/.my.cnf    ./
+cp ~/.profile   ./
+cp ~/.spring.rb ./
+cp ~/.tmux.conf ./
+cp ~/.vimrc     ./
+cp ~/.zshrc     ./
+cp ~/.zlogin    ./
